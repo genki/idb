b92fac115fe2477c9299054666a0d97d89986a0c
takiuchi <genki@s21g.com>
2009-02-07 19:31:59 +0900

    Prepared skelton of idb.

--- lib/idb.rb
+++ lib/idb.rb
@@ -1,4 +1,20 @@
+require 'irb'
 
 class Idb
   VERSION = '0.0.1'
+
+  def self.run(options = {})
+    IRB.setup nil
+    IRB.conf[:PROMPT][:IDB] = {
+      :PROMPT_I => "idbI>> ",
+      :PROMPT_N => "idbN> ",
+      :PROMPT_S => "idbS> ",
+      :PROMPT_C => "idbC> ",
+    }
+    irb = IRB::Irb.new
+    IRB.conf[:MAIN_CONTEXT] = irb.context
+
+    trap(:INT){irb.signal_handle}
+    catch(:IRB_EXIT){irb.eval_input}
+  end
 end
